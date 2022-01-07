941c1e89aede949ff6fddeb23bb93f0f743b7671
VM7788 <vm7788@mail.ru>
2022-01-08 00:22:15 +0500

    first

--- src/main.java
+++ src/main.java
@@ -1,4 +1,11 @@
 public class main {
 
+    public static void main(String[] args) {
+        int i = 1;
+        int y = 2;
 
+        System.out.println(i);
+        System.out.println(y);
+
+    }
 }
